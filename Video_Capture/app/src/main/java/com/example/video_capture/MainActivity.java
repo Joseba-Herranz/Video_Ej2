833dbef4e3967821552d319a09b412db500a44f8
Joseba Andoni Herranz <josdron2@gmail.com>
2023-11-12 14:11:57 +0100

    main

---
+++ Video_Capture/app/src/main/java/com/example/video_capture/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.video_capture;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_VIDEO_CAPTURE = 1;
+    private Uri videoUri;
+    private Button btnRecord;
+    private VideoView videoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnRecord = findViewById(R.id.btn_record);
+        videoView = findViewById(R.id.videoView);
+        btnRecord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakeVideoIntent(v);
+            }
+        });
+    }
+
+    public void dispatchTakeVideoIntent(View view) {
+        Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        if (takeVideoIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
+            videoUri = data.getData();
+            videoView.setVideoURI(videoUri);
+            videoView.start();
+        }
+    }
+}
